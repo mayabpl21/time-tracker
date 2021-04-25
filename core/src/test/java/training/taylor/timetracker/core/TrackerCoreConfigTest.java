cbfa787ad59b1e3d5c56019ae02b1ffd3139dce5
mayabpl21 <83155386+mayabpl21@users.noreply.github.com>
2021-04-25 12:38:23 +0530

    testing

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -1,9 +1,9 @@
 package training.taylor.timetracker.core;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
